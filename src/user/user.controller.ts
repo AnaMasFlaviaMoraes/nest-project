88f1d8cdd46af39d006c5b37d6f257f18ddf9546
AnaMasFlaviaMoraes <anaflavia.chamoraes@gmail.com>
2025-06-25 22:13:48 -0300

    Criando estrutura inicial

---
+++ src/user/user.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, Post, Body, Param, Patch, Delete } from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  create(@Body() body: { name: string; email: string; password: string }) {
+    return this.userService.create(body);
+  }
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() body: { name?: string; email?: string },
+  ) {
+    return this.userService.update(+id, body);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.userService.remove(+id);
+  }
+}
